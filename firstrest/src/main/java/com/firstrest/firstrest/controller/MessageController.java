720f1360d204390037d3a55559328dc65c3e2737
Ulyana Cheshko <ylyanacheshko@gmail.com>
2022-11-09 10:43:14 +0300

    Initial commit

---
+++ firstrest/src/main/java/com/firstrest/firstrest/controller/MessageController.java
@@ -0,0 +1,33 @@
+package com.firstrest.firstrest.controller;
+
+import com.firstrest.firstrest.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("calculateTwoInt")
+public class MessageController {
+    @GetMapping("+/{num1}/{num2}")
+    public Integer Plus(@PathVariable Integer num1, @PathVariable Integer num2) {
+        Integer result = num1 + num2;
+        return result;
+    }
+    @GetMapping("-/{num1}/{num2}")
+    public Integer Minus(@PathVariable Integer num1, @PathVariable Integer num2) {
+        Integer result = num2 - num1;
+        return result;
+    }
+
+    @GetMapping("*/{num1}/{num2}")
+    public Integer Multiply(@PathVariable Integer num1, @PathVariable Integer num2) {
+       Integer result = num1 * num2;
+        return result;
+    }
+
+    @GetMapping(":/{num1}/{num2}")
+    public Integer Divide(@PathVariable Integer num1, @PathVariable Integer num2) {
+        if(num2.equals(0)){return 0;}
+        Integer result = num2 / num1;
+        return result;
+    }
+}
